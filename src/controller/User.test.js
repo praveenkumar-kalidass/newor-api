670ce103b1dc7537451758d04400a669be4faef4
Praveenkumar Kalidass <praveen.ece@outlook.com>
2022-07-05 12:45:15 +0530

    #3: Fix tests for auth token in Authorize flow

--- src/controller/User.test.js
+++ src/controller/User.test.js
@@ -2,7 +2,6 @@ const httpMocks = require('node-mocks-http');
 
 const neworError = require('../constant/error');
 const userService = require('../service/User');
-const clientService = require('../service/Client');
 const userController = require('./User');
 
 jest.mock('../service/User', () => ({
@@ -11,6 +10,13 @@ jest.mock('../service/User', () => ({
 }));
 jest.mock('../service/Client', () => ({
   authorize: jest.fn(),
+}));
+jest.mock('../helper/oauth', () => ({
+  getModel: () => ({
+    getClient: jest.fn((id) => ({ id, grants: ['password', 'authorization_code'], redirectUris: ['authorize'] })),
+    saveAuthorizationCode: jest.fn((code) => code),
+    saveToken: jest.fn((accessToken, client, user) => ({ ...accessToken, client, user })),
+  }),
 }));
 
 describe('User Controller', () => {
@@ -88,11 +94,6 @@ describe('User Controller', () => {
 
     it('should redirect authorisation code as success response', async () => {
       userService.login.mockResolvedValueOnce({ id: 'user_id' });
-      clientService.authorize.mockResolvedValueOnce({
-        id: 'client_id',
-        grants: ['password', 'authorization_code'],
-        redirectUris: ['authorize'],
-      });
 
       await userController.loginV1(requestMock, responseMock);
 
